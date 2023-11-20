c157ef1a71b0b63671070c66561eda1e872644ca
Ritishree-12 <ritishree1996@gmail.com>
2023-11-20 16:29:51 +0530

    myapp

--- src/authentication/Welcome.js
+++ src/authentication/Welcome.js
@@ -1,27 +1,48 @@
-import { StyleSheet, Text, View, TextInput,Image } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
+import React, { useState } from "react";
 
 const Welcome = () => {
+  const [activeButton, setActiveButton] = useState(null);
+
+  const handleButtonPress = (buttonName) => {
+    setActiveButton(buttonName === activeButton ? null : buttonName);
+  };
+
   return (
     <View
       style={[
         styles.container,
         {
-          // Try setting `flexDirection` to `"row"`.
           flexDirection: "column",
         },
       ]}
     >
       <View style={{ flex: 4, backgroundColor: "white" }}>
-        <Image style={styles.image} source={require('../../assets/tracking_logo.png')} />
+        <Image style={styles.image} source={require('../../assets/welcome.png')} />
       </View>
       <View style={{ flex: 2, backgroundColor: "white" }}>
         <Text style={styles.text1}>Welcome</Text>
         <Text style={styles.text2}>Have a better sharing experience</Text>
       </View>
       <View style={{ flex: 2.5, backgroundColor: "white" }}>
-        <TextInput style={styles.input} placeholder="Register" />
-        <TextInput style={styles.input} placeholder="Sign Up" />
+        <TouchableOpacity
+          style={[
+            styles.button,
+            activeButton === "Register" && styles.active,
+          ]}
+          onPress={() => handleButtonPress("Register")}
+        >
+          <Text style={[styles.buttonText, activeButton === "Register" && styles.textWhite]}>Register</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[
+            styles.button,
+            activeButton === "Signup" && styles.active,
+          ]}
+          onPress={() => handleButtonPress("Signup")}
+        >
+          <Text style={[styles.buttonText, activeButton === "Signup" && styles.textWhite]}>Sign Up</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -32,25 +53,29 @@ export default Welcome;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // padding: 20,
   },
-  input: {
+  button: {
     height: 60,
     margin: 8,
     borderWidth: 1,
     padding: 12,
     borderRadius: 40,
     borderColor: "red",
-    color: "red",
-    textAlign: "center",
-    // fontWeight: "bold",
-    fontSize: 17,
+    backgroundColor: "transparent",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  hover: {
+  active: {
     backgroundColor: "red",
   },
+  textWhite: {
+    color: "white",
+    fontSize:18,
+  },
+  buttonText: {
+    color: "red", // Default text color
+  },
   text1: {
-    // fontWeight: "bold",
     textAlign: "center",
     fontWeight: "600",
     fontSize: 28,
@@ -61,12 +86,10 @@ const styles = StyleSheet.create({
     margin: 13,
     color: "grey",
   },
-  image:{
-    height:270,
-    //  margin:50,
-    alignSelf:'center',
-    marginTop:60,
-    // padding:20,
-    width:270
-  }
+  image: {
+    height: 270,
+    alignSelf: 'center',
+    marginTop: 60,
+    width: "100%",
+  },
 });
